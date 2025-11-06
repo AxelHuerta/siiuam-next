85a8cd657c18678aec50ff5bb11a8af810d6b972
Axel Huerta <axelhuerta.dev@gmail.com>
2025-11-06 13:57:06 -0600

    add function for get crc value

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,69 +1,26 @@
 import axios, { AxiosResponse } from "axios";
 import { NextResponse } from "next/server";
+import * as cheerio from "cheerio";
 
 export async function POST(request: Request) {
   try {
     const { matricula, password } = await request.json();
 
-    const sessionResponse: AxiosResponse = await axios.get(
-      "https://zacatlan.rec.uam.mx:8443/rec/izt/AEWBU004.oIniSesWebLic?mod=1",
-      {
-        withCredentials: true,
-        maxRedirects: 0,
-        validateStatus: (s) => s < 400,
-        headers: {
-          "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0 Safari/537.36",
-          Accept:
-            "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-          "Accept-Language": "es-MX,es;q=0.9,en;q=0.8",
-          Referer: "https://zacatlan.rec.uam.mx:8443/rec/izt/",
-          "Upgrade-Insecure-Requests": "1",
-          Connection: "keep-alive",
-        },
-      }
-    );
+    const cookies = await getInitialCookies();
+    if (cookies.length < 3)
+      return NextResponse.json("Error getting cookies", { status: 500 });
 
-    const initialCookies = sessionResponse.headers[`set-cookie`];
-
-    if (!initialCookies || initialCookies?.length === 0) {
-      return NextResponse.json({ error: "No cookies found" }, { status: 500 });
-    }
-    const parsedCookies = parseCookies(initialCookies);
-
-    const framesResponse = await axios.get(
-      "https://zacatlan.rec.uam.mx:8443/rec/izt/AEWBF003.oMuestraFrames?mod=1",
-      {
-        withCredentials: true,
-        headers: {
-          "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
-          cookie: `${parsedCookies[0]}; ${parsedCookies[1]}`,
-          Referer: "https://zacatlan.rec.uam.mx:8443/rec/izt/",
-          "Upgrade-Insecure-Requests": "1",
-          Connection: "keep-alive",
-        },
-      }
-    );
-
-    const frameCookies = framesResponse.headers[`set-cookie`];
-
-    if (!frameCookies || frameCookies?.length === 0) {
-      return NextResponse.json(
-        { error: "No frame cookies found" },
-        { status: 500 }
-      );
-    }
-    const formattedFrameCookies = parseCookies(frameCookies);
+    const crc = await getCrcValue(cookies[0]);
+    if (!crc)
+      return NextResponse.json("Error getting CRC value", { status: 500 });
 
     const bodyParams = `
-SIGLAS_UNI_XX.E_UNIDAD.AE02.1=IZT&%23.E_UNIDAD.AE02.1=AxJDMg%3D%3D&%23CRC.E_UNIDAD.AE02.1=00000054&NOMBRE.IDENTIFICACION.NONMODELED=${matricula}&COMPLEMENTO.IDENTIFICACION.NONMODELED=${password}&GO.IDENTIFICACION.NONMODELED=Entrar&%25.IDENTIFICACION.NONMODELED=&%23.WEB_INFO.SW01=&%23.WEB_MOD_ASO.SW01=&%23.USUARIO_ANEXO.SG02=&%23.MODULO_UWAS.SAE01=,      
-
-`;
+SIGLAS_UNI_XX.E_UNIDAD.AE02.1=IZT&%23.E_UNIDAD.AE02.1=AxJDMg%3D%3D&%23CRC.E_UNIDAD.AE02.1=${crc}&NOMBRE.IDENTIFICACION.NONMODELED=${matricula}&COMPLEMENTO.IDENTIFICACION.NONMODELED=${password}&GO.IDENTIFICACION.NONMODELED=Entrar&%25.IDENTIFICACION.NONMODELED=&%23.WEB_INFO.SW01=&%23.WEB_MOD_ASO.SW01=&%23.USUARIO_ANEXO.SG02=&%23.MODULO_UWAS.SAE01=, `;
 
     const headers = {
       accept:
         "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+
       "accept-language": "en-US,en;q=0.9,es;q=0.8",
       "cache-control": "max-age=0",
       "content-type": "application/x-www-form-urlencoded",
@@ -75,7 +32,7 @@ SIGLAS_UNI_XX.E_UNIDAD.AE02.1=IZT&%23.E_UNIDAD.AE02.1=AxJDMg%3D%3D&%23CRC.E_UNID
       "sec-fetch-site": "same-origin",
       "sec-fetch-user": "?1",
       "upgrade-insecure-requests": "1",
-      cookie: `${formattedFrameCookies[0]}; ${parsedCookies[1]}; ${parsedCookies[0]}`,
+      cookie: `${cookies[2]}; ${cookies[1]}; ${cookies[0]}`,
       Referer:
         "https://zacatlan.rec.uam.mx:8443/rec/izt/AEWBU004.oIniSesWebLic?mod=1",
     };
@@ -109,7 +66,8 @@ SIGLAS_UNI_XX.E_UNIDAD.AE02.1=IZT&%23.E_UNIDAD.AE02.1=AxJDMg%3D%3D&%23CRC.E_UNID
 
     const isLogged = loginResponse.data.includes("Kardex");
     const loginCookies = loginResponse.headers[`set-cookie`];
-    loginCookies?.push(initialCookies[0]);
+
+    loginCookies?.push(cookies[0]);
     const loginParsedCookies = parseCookies(
       loginResponse.headers["set-cookie"] || []
     );
@@ -163,4 +121,95 @@ function formatCookies(cookieArray: string[]) {
   }
 
   return cookieObj;
+}
+
+async function getInitialCookies(): Promise<string[]> {
+  const sessionResponse: AxiosResponse = await axios.get(
+    "https://zacatlan.rec.uam.mx:8443/rec/izt/AEWBU004.oIniSesWebLic?mod=1",
+    {
+      withCredentials: true,
+      maxRedirects: 0,
+      validateStatus: (s) => s < 400,
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0 Safari/537.36",
+        Accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "Accept-Language": "es-MX,es;q=0.9,en;q=0.8",
+        Referer: "https://zacatlan.rec.uam.mx:8443/rec/izt/",
+        "Upgrade-Insecure-Requests": "1",
+        Connection: "keep-alive",
+      },
+    }
+  );
+
+  const initialCookies = sessionResponse.headers[`set-cookie`];
+
+  if (!initialCookies || initialCookies?.length === 0) {
+    return [];
+  }
+
+  let parsedInitialCookies = parseCookies(initialCookies);
+
+  const framesResponse = await axios.get(
+    "https://zacatlan.rec.uam.mx:8443/rec/izt/AEWBF003.oMuestraFrames?mod=1",
+    {
+      withCredentials: true,
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
+        cookie: `${parsedInitialCookies[0]}; ${parsedInitialCookies[1]}`,
+        Referer: "https://zacatlan.rec.uam.mx:8443/rec/izt/",
+        "Upgrade-Insecure-Requests": "1",
+        Connection: "keep-alive",
+      },
+    }
+  );
+
+  const frameCookies = framesResponse.headers[`set-cookie`];
+
+  if (!frameCookies || frameCookies?.length === 0) {
+    return [];
+  }
+
+  const formattedFrameCookies = parseCookies(frameCookies);
+  parsedInitialCookies = parsedInitialCookies.concat(formattedFrameCookies);
+
+  return parsedInitialCookies;
+}
+
+async function getCrcValue(cookie: string) {
+  const crcUrl =
+    "https://zacatlan.rec.uam.mx:8443/rec/izt/AEWBU004.oIniSesWebLic?mod=1";
+
+  const crcHeaders = {
+    headers: {
+      accept:
+        "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+      "accept-language": "en-US,en;q=0.9,es;q=0.8",
+      "sec-ch-ua": '"Not_A Brand";v="99", "Chromium";v="142"',
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": '"Linux"',
+      "sec-fetch-dest": "frame",
+      "sec-fetch-mode": "navigate",
+      "sec-fetch-site": "same-origin",
+      "upgrade-insecure-requests": "1",
+      cookie: `UwdSessionID=MODULOUAM=1; ${cookie}`,
+      Referer:
+        "https://zacatlan.rec.uam.mx:8443/rec/izt/AEWBF003.oMuestraFrames?mod=1",
+    },
+  };
+
+  const crcRequest = await axios
+    .get(crcUrl, crcHeaders)
+    .then((res) => res.data)
+    .catch((err) => {
+      console.error("CRC ERROR:", err.response?.status, err.response?.data);
+      return null;
+    });
+
+  const $ = cheerio.load(crcRequest || "");
+  const crcValue = $('[name="#CRC.E_UNIDAD.AE02.1"]').val()?.toString() || "";
+
+  return crcValue;
 }
